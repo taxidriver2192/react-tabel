e36dbb9fcbaaf612eb9e0b2dc4955617870d158b
Lukas Jürs Schmidt <mr.lukas.schmidt@gmail.com>
2022-02-25 09:25:43 +0100

    update
    Fundet ud af hvordan man bruger table ...

---
+++ src/components/columns.js
@@ -0,0 +1,26 @@
+export const COLUMNS = [
+    {
+        Header: 'Id',
+        accessor: 'id'
+    },
+    {
+        Header: 'First Name',
+        accessor: 'first_name'
+    },
+    {
+        Header: 'Last Name',
+        accessor: 'last_name'
+    },
+    {
+        Header: 'Date of Birth',
+        accessor: 'Date_of_Birth'
+    },
+    {
+        Header: 'Country',
+        accessor: 'Country'
+    },
+    {
+        Header: 'Phone',
+        accessor: 'Phone'
+    },
+]
